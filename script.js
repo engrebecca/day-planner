88092f0a4a40c0c606c81d9d9071a3b5fa6c4ca5
reng <67654603+engrebecca@users.noreply.github.com>
2020-09-25 14:49:32 -0700

    Add on click function for save button

--- script.js
+++ script.js
@@ -11,9 +11,13 @@ function setPage(){
     }
 
     // get object from local storage and JSON parse
+    var newArr = [9,10,11,12,13,14,15,16,17]
+    for (var i = 0; i < timeArr.length; i++)
+    // Access key of object
+    eventOb[i + "AM"]
 }
-console.log(moment().format("HH"));
-setPage();
+// console.log(moment().format("HH"));
+// setPage();
 
 // When timeblock clicked, an event/appt can be entered
 // $(".eventblock").on("click", function(){
@@ -25,9 +29,9 @@ var eventOb = {};
 $(".saveBtn").on("click", function(){
     var eventTime = $(this).prev().prev().html();
     console.log(eventTime);
-    eventOb.eventTime = $(this).prev().html();
+    eventOb[eventTime] = $(this).prev().html();
     console.log(eventOb);
 
-    eventTime = $(this).prev().html()
-    localStorage.setItem("event", eventTime);
+    // use JSON stringify to convert object to string
+    localStorage.setItem("event", JSON.stringify(eventOb));
 })
